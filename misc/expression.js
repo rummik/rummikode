a9252bc909864f31b4da229628a478459cfaa541
rummik <k@9k1.us>
2011-10-18 14:27:14 -0400

    Fixed operation order, parentheses, and precision adjustments.

--- misc/expression.js
+++ misc/expression.js
@@ -1,17 +1,28 @@
 Math.eval = function(expression, precision) {
-	var i, p, start, regex, last, operator, operation, expr,
+	precision = Math.max(0, Math.min(10, typeof precision == 'undefined' ? 4 : parseInt(precision)));
+	expression = expression.replace(/\s+/g, '');
+
+	var i, p, start, regex, last, operator, operation, expr = '',
 	    number = '([+-]?(?:\\d+(?:\\.\\d*)?|\\.\\d+))',
+	    psh = function(n) {
+		    n = new String(n);
+		    n = n.split('.');
+		    return +((n[0] || '') + (n[1] || '').substr(0, precision) + (new Array(Math.max(0, precision - (n[1] || '').length) + 1).join('0')));
+	    },
+
+	    pus = function(n) {
+		    n = new String(n);
+		    return +((n.substr(0, n.length - precision) + '.' + n.substr(n.length - precision)).replace(/\.?0*$/, ''));
+	    },
+
 	    operators = {
 		'^': function(a, b) { return Math.pow(a, b); },
 		'*': function(a, b) { return a * b; },
 		'/': function(a, b) { return a / b; },
-		'+': function(a, b) { return a + b; },
-		'-': function(a, b) { return a - b; },
-	};
+		'+': function(a, b) { return pus(psh(a) + psh(b)); },
+		'-': function(a, b) { return pus(psh(a) - psh(b)); },
+	    };
 
-	expr = expression = expression.replace(/\s+/g, '');
-
-	precision = Math.max(0, Math.min(12, typeof precision == 'undefined' ? 4 : parseInt(precision)));
 
 	for (p=i=0; i<expression.length; i++) {
 		switch (expression[i]) {
@@ -26,22 +37,22 @@ Math.eval = function(expression, precision) {
 				if (--p > 0)
 					continue;
 
-				expr = expr.substr(1, (start - (expression.length - expr.length)) - 1)
-				     + arguments.callee(expression.substr(start, i - start), precision)
-				     + expression.substr(i + 1);
+				expr += arguments.callee(expression.substr(start, i - start), precision);
 
 				start = undefined;
+				break;
+
+			default:
+				if (p == 0)
+					expr += expression.charAt(i);
 				break;
 		}
 	}
 
-	expression = expr.replace(new RegExp(number, 'g'), function(n) {
-		n = n.split('.');
-		return (n[0] || '') + (n[1] || '').substr(0, precision) + (new Array(Math.max(0, precision - (n[1] || '').length) + 1).join('0'));
-	});
+	expression = expr;
 
 	for (operator in operators) {
-		regex = new RegExp('([^+-][+-])?' + number + operator.replace(/([*+^])/, '\\$1') + number, 'g');
+		regex = new RegExp('([^+-][+-])??' + number + operator.replace(/([*+^])/, '\\$1') + number);
 		operation = function(m, o, a, b) { return (o || '').toString() + operators[operator](+a, +b); };
 		for (last=undefined; last!=expression; last=expression, expression=expression.replace(regex, operation));
 	}
@@ -49,7 +60,5 @@ Math.eval = function(expression, precision) {
 	if (!new RegExp('^' + number + '$').test(expression))
 		return false;
 
-	return expression.replace(new RegExp(number, 'g'), function(n) {
-		return (n.substr(0, n.length - precision) + '.' + n.substr(n.length - precision)).replace(/\.?0*$/, '');
-	});
+	return +expression;
 };
